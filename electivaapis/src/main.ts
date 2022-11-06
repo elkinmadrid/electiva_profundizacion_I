3af06fc108fb29206e9149a80e79812adf478b4b
Elkin Madrid <elkinamp@gmail.com>
2022-11-05 23:14:14 -0500

    Documentacion Swagger y despligue en Heroku

--- electivaapis/src/main.ts
+++ electivaapis/src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors()
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Ignorar datos que no esten en los dto
+      forbidNonWhitelisted: true // Lanzar error si existen datos prohibidos
+    })
+  )
+
+  const config = new DocumentBuilder()
+    .setTitle('API - Electiva Prof I')
+    .setDescription('Elkin Madrid, Andrea Arevalo y Victor Arzuza')
+    .setContact('Elkin Madrid', 'elkinmadrid.co', 'eamadridp@ul.edu.co')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- electivaapis/src/users/dto/create-user.dto.ts
+++ electivaapis/src/users/dto/create-user.dto.ts
@@ -1,16 +1,66 @@
+import { IsBoolean, IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+
+
 export class CreateUserDto {
+
+    @IsEmail()
+    @IsNotEmpty()
+    @ApiProperty({description: 'The email of the user'})
     readonly email: string;
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({description: 'The password of the user'})
     password: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty({description: 'The name of the user'})
     readonly name: string;
-    readonly lastname?: string;
-    readonly identification?: string;
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty({description: 'The lastname of the user'})
+    readonly lastname: string;
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty({description: 'The identification of the user'})
+    readonly identification: string;
+
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({description: 'The photo of the user'})
     readonly pinture?: string;
+    @IsString()
+    @IsOptional()
+    @ApiProperty({description: 'The number phone of the user'})
     readonly phone?: string;
+    @IsString()
+    @IsOptional()
+    @ApiProperty({description: 'The gender of the user'})
     readonly gender?: string;
+    @IsString()
+    @IsOptional()
+    @ApiProperty({description: 'The birthday of the user'})
     readonly birthday?: string;
+    @IsString()
+    @IsOptional()
+    @ApiProperty({description: 'The country of the user'})
     readonly country?: string;
+    @IsString()
+    @IsOptional()
+    @ApiProperty({description: 'The state of the user'})
     readonly state?: string;
+    @IsString()
+    @IsOptional()
+    @ApiProperty({description: 'The city of the user'})
     readonly city?: string;
+    @IsString()
+    @IsOptional()
+    @ApiProperty({description: 'The address of the user'})
     readonly address?: string;
+    @IsBoolean()
+    @IsOptional()
+    @ApiProperty({description: 'User is active'})
     readonly active?: boolean;
 }
